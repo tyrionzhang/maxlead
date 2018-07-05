59f85a16724fa7c7c4eec182bc26d93d2ad826d7
tyrionzhang <swlxyztd@163.com>
2018-07-05 10:02:53 +0800

    chrome

--- bots/stockbot/stockbot/spiders/hanover_spiders.py
+++ bots/stockbot/stockbot/spiders/hanover_spiders.py
@@ -29,10 +29,10 @@ class HanoverSpider(scrapy.Spider):
     def parse(self, response):
         file_path = os.path.join(max_settings.BASE_DIR, max_settings.THRESHOLD_TXT, 'threshold_txt.txt')
         msg_str2 = ''
-        fireFoxOptions = webdriver.FirefoxOptions()
-        fireFoxOptions.add_argument('-headless')
-        fireFoxOptions.add_argument('--disable-gpu')
-        driver = webdriver.Firefox(firefox_options=fireFoxOptions)
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=settings.CHROME_PATH)
         driver.get(response.url)
         elem_code = driver.find_elements_by_id('WarehouseCode')
         elem_acode = driver.find_elements_by_id('AccountCode')
