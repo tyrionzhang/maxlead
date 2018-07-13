ebb301df15299c96778a0893773de10bf3f1eac1
tyrionzhang <swlxyztd@163.com>
2018-07-13 16:23:04 +0800

    运行爬虫

--- max_stock/models.py
+++ max_stock/models.py
@@ -37,3 +37,40 @@ class StockLogs(models.Model):
 
     class Meta:
         db_table = 'stock_logs'
+
+class AmazonCode(models.Model):
+    email = models.CharField('Email',max_length=225)
+    code = models.CharField('Code',max_length=225)
+    created = models.DateTimeField('Create Date', auto_now_add=True)
+
+    class Meta:
+        db_table = 'amazon_code'
+
+class OrderItems(models.Model):
+    order_id = models.CharField('Order Id', max_length=225)
+    sku = models.CharField('SKU', max_length=50)
+    order_status = models.CharField('Status', max_length=50)
+    email = models.CharField('Email', max_length=50)
+    is_email = models.IntegerField('Is Email', default=0)
+    created = models.DateTimeField('Create Date', auto_now_add=True)
+
+    class Meta:
+        db_table = 'order_items'
+
+class Templates(models.Model):
+    sku = models.CharField('SKU', max_length=50)
+    content = models.TextField('Content')
+    created = models.DateTimeField('Create Date', auto_now_add=True)
+
+    class Meta:
+        db_table = 'templates'
+
+class Schedule(models.Model):
+    templates = models.ForeignKey(Templates, on_delete=models.CASCADE)
+    sku = models.CharField('SKU', max_length=50)
+    time_str = models.CharField('Time Str', max_length=50)
+    created = models.DateTimeField('Create Date', auto_now_add=True)
+
+    class Meta:
+        db_table = 'schedule'
+
